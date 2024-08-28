24486af117d21a870f8c0b43428a9f3d008aec0e
taechichi <canon082828@gmail.com>
2024-08-28 11:30:13 +0900

    init project commit

---
+++ src/main/java/com/scit/proj/scitsainanguide/ScitSainanGuideApplication.java
@@ -0,0 +1,13 @@
+package com.scit.proj.scitsainanguide;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScitSainanGuideApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScitSainanGuideApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/scit/proj/scitsainanguide/ScitSainanGuideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scit.proj.scitsainanguide;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScitSainanGuideApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
