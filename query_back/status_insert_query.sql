a665189f206be2e9feeda3245aa6cb006702aa00
PJH <sk10101@naver.com>
2024-08-30 10:54:21 +0900

    status 테이블 데이터 insert 쿼리 백업, 쿼리 보관 디렉토리 수정

---
+++ query_back/status_insert_query.sql
@@ -0,0 +1,10 @@
+-- 한국어
+INSERT INTO status (status_name, lang_cd)　VALUES('평상시', 'kr');
+INSERT INTO status (status_name, lang_cd)　VALUES('안전', 'kr');
+INSERT INTO status (status_name, lang_cd)　VALUES('대피중', 'kr');
+INSERT INTO status (status_name, lang_cd)　VALUES('위험', 'kr');
+-- 日本語
+INSERT INTO status (status_name, lang_cd)　VALUES('平常', 'jp');
+INSERT INTO status (status_name, lang_cd)　VALUES('安全', 'jp');
+INSERT INTO status (status_name, lang_cd)　VALUES('避難中', 'jp');
+INSERT INTO status (status_name, lang_cd)　VALUES('緊急', 'jp');
