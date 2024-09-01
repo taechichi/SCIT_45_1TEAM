002866db017c0c63a20a2d98a02353cedb0159c5
PJH <sk10101@naver.com>
2024-09-01 19:30:12 +0900

    .마이 페이지 > 친구 관리 > 친구 즐겨찾기 기능 추가 및 코드 정리

--- src/main/java/com/scit/proj/scitsainanguide/repository/MyFriendRepository.java
+++ src/main/java/com/scit/proj/scitsainanguide/repository/MyFriendRepository.java
@@ -6,4 +6,6 @@ import org.springframework.data.domain.Page;
 public interface MyFriendRepository {
 
     Page<FriendDTO> selectMyFirendList(int page, int pageSize, String searchType, String searchWord, String memberId);
+
+    void updateFriend(String memberId, String friendId);
 }
