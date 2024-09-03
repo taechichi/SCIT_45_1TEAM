c71ebf250d676de9c609821f1ba8d13495e21324
yaree0927 <yaree0927@gmail.com>
2024-09-04 01:28:03 +0900

    Update_Geocoder

--- src/main/java/com/scit/proj/scitsainanguide/service/HospitalService.java
+++ src/main/java/com/scit/proj/scitsainanguide/service/HospitalService.java
@@ -43,19 +43,10 @@ public class HospitalService {
             //log.debug("name: {}", node.path("設・場所名").asText());
 
             HospitalEntity hospitalEntity = HospitalEntity.builder()
-                    // 유성상이 수정해야할 곳~ :)
-                    /*.shelterName(node.path("設・場所名").asText())
+                    .hospitalName(node.path("設・場所名").asText())
                     .latitude(node.path("緯度").asText())
                     .longitude(node.path("経度").asText())
-                    .floodYn("1".equals(node.path("洪水").asText(""))) // "1"일 때 true, 아니면 false
-                    .earthYn("1".equals(node.path("崖崩れ、土石流及び地滑り").asText("")))
-                    .highTideYn("1".equals(node.path("高潮").asText("")))
-                    .earthquakeYn("1".equals(node.path("地震").asText("")))
-                    .tsunamiYn("1".equals(node.path("津波").asText("")))
-                    .fireYn("1".equals(node.path("大規模な火事").asText("")))
-                    .inlandFloodingYn("1".equals(node.path("内水氾濫").asText("")))
-                    .volcanoYn("1".equals(node.path("火山現象").asText("")))
-                    .lang_cd("ja") // 고정 값*/
+                    .langCd("ja") // 고정 값*/
                     .build();
             // DB에 저장
             hospitalRepository.save(hospitalEntity);
