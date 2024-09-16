1cdb0e3f94e1fd4a6fca78190cc6a54be9eb1366
taechichi <canon082828@gmail.com>
2024-09-16 09:59:13 +0900

    commit for pull

--- src/main/java/com/scit/proj/scitsainanguide/repository/MarkerFavoritesJPARepository.java
+++ src/main/java/com/scit/proj/scitsainanguide/repository/MarkerFavoritesJPARepository.java
@@ -2,6 +2,8 @@ package com.scit.proj.scitsainanguide.repository;
 
 import com.scit.proj.scitsainanguide.domain.entity.MarkerFavoritesEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface MarkerFavoritesJPARepository extends JpaRepository<MarkerFavoritesEntity, Integer> {
 }
--- src/main/resources/static/js/myPage/myMarkerFavorites.js
+++ src/main/resources/static/js/myPage/myMarkerFavorites.js
@@ -15,7 +15,6 @@ $(document).ready(function() {
         myLong = position.coords.longitude;
     })*/
 
-
     // 필터 버튼 클릭 시 필터 조건을 설정하고 폼을 제출 ====================================
     $('#filterAll').click(function() {
         $('#filter').val(''); // 필터를 제거
@@ -35,7 +34,7 @@ $(document).ready(function() {
         fetchMarkers();
     });
 
-    // 검색어 입력 시 실시간 검색 (keyup 이벤트) ====================================
+    // 검색어 입력 시 실시간 검색 (keyup 이벤트) =========================================
     $('#searchWord').on('keyup', function() {
         //$('#page').val(1); // 페이지를 1로 초기화
         fetchMarkers();
@@ -53,6 +52,55 @@ $(document).ready(function() {
         //$('#page').val(1); // 페이지를 1로 초기화
         fetchMarkers();
     });
+
+    // 닉네임 수정 / 등록 기능 ===========================================================
+    $('#markerTableBody').on('click', 'a.nickname-exists', function (event) {
+        event.preventDefault();
+        let favoriteId = $(this).closest('tr').find('input[name="favoriteId"]').val();
+        let currentNickname = $(this).text();
+
+        if(confirm(`닉네임을 수정하시겠습니까?\n현재 닉네임: ${currentNickname}`)) {
+            let newNickname = prompt('새로운 닉네임을 입력하세요:', currentNickname);
+            if(newNickname !== null) {
+                updateNickname(favoriteId, newNickname);
+            }
+        }
+    });
+
+    // 닉네임이 없는 경우 클릭 시 등록할 수 있는 알림창을 띄움
+    $('#markerTableBody').on('click', 'a.nickname-missing', function(event) {
+        event.preventDefault(); // 기본 링크 클릭 동작 방지
+        let favoriteId = $(this).closest('tr').find('input[name="favoriteId"]').val();
+
+        if (confirm('새로운 닉네임을 지으시겠습니까?')) {
+            let newNickname = prompt('새로운 닉네임을 입력하세요:');
+            if (newNickname) {
+                updateNickname(favoriteId, newNickname);
+            }
+        }
+    });
+
+    // 닉네임 수정/등록을 서버로 전달하는 함수
+    function updateNickname(favoriteId, newNickname) {
+        $.ajax({
+            url: '/my/myMarkerFavorites/updateNickname',
+            type: 'POST',
+            data: {
+                favoriteId: favoriteId,
+                nickname: newNickname,
+            },
+            success: function(response) {
+                fetchMarkers();
+                alert('닉네임이 성공적으로 업데이트되었습니다. ');
+            },
+            error: function (error) {
+                alert('닉네임 업데이트 중 오류가 발생했습니다. ');
+                console.log('Error:', error);
+            }
+        });
+    }
+
+    // =================================================================================
 
     // Ajax로 리스트 데이터를 불러오는 함수
     function fetchMarkers() {
