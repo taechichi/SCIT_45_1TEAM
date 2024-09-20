fc35375057cc56ce14a5b395b54418a12fd48364
gunhee <dpfaps00@naver.com>
2024-09-20 21:09:51 +0900

    국제화수정

--- src/main/resources/static/js/myPage/message/myMessage.js
+++ src/main/resources/static/js/myPage/message/myMessage.js
@@ -15,11 +15,6 @@ $(document).ready(function () {
         pagingFormSubmit(1); // 여기서 pagingFormSubmit 함수 호출
     });
 
-    // 버튼 클릭 시 활성화 효과
-    $('#writeMessageBtn').on('click', function() {
-        location.href = '/my/message/write'; // 글쓰기 페이지로 이동
-    });
-
     $('#deleteMessageBtn').on('click', function() {
         let selectedMessageIdList = [];
         $('.messageCheckbox:checked').each(function () {
@@ -54,3 +49,15 @@ $(document).ready(function () {
         $('.messageCheckbox').prop('checked', isChecked);
     });
 });
+$(document).ready(function(){
+
+    // 친구 검색 버튼을 눌렀을 때 새 창을 열어 친구 목록 검색 창 띄움
+    $('#writeMessageBtn').click(function(){
+        window.open(	// 현재 창(부모창) 기준 자식창 새로 열기 - method="get"만 지원, "post"로 하고 싶으면, js 사용해야!
+            // open() 함수: 괄호 안 4Param - 경로, 새로 열릴 창 이름(자유), value(default: _blank 새 창), 모양
+            // http://localhost:8888/my/message/searchFriend -> 그치만 이렇게 열면, 부모창-자식창 관계가 성립되지 않음!
+            '/my/message/write',
+            'write',
+            'top=150, left=150, width=600, height=450, location=no, titlebar=no');	// 시작 위치 & 크기
+    })
+})
