129e4dc1c9f5d969d6960272bd8dc841af1267b1
PJH <sk10101@naver.com>
2024-09-27 16:32:41 +0900

    404 error 페이지도 초안도 추가, sql 백업본에 불필요한 내용 제거

--- query_back/alarm_category_insert_query.sql
+++ query_back/alarm_category_insert_query.sql
@@ -2,5 +2,3 @@ INSERT INTO alarm_category(category_name, category_name_ja) VALUES('쪽지알림
 INSERT INTO alarm_category(category_name, category_name_ja) VALUES('친구신청알림', '友達申請通知');
 INSERT INTO alarm_category(category_name, category_name_ja) VALUES('친구수락알림', '友達承認通知');
 INSERT INTO alarm_category(category_name, category_name_ja) VALUES('긴급상태알림', '緊急状態通知');
-INSERT INTO friend_member (member_id, friend_id, friend_yn, favorite_yn, request_dt)
-VALUES ('yyy', 'yyy', true, true, NOW());
