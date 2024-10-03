ed0fcfbfc29dae73e9c6ac921591d26e0e5e27af
PJH <sk10101@naver.com>
2024-10-03 15:38:24 +0900

    status insert query 수정 (status name 수정 내용 적용)

--- query_back/status_insert_query.sql
+++ query_back/status_insert_query.sql
@@ -1,5 +1,5 @@
 -- 한국어, 日本語
 INSERT INTO status (status_name, status_name_ja) VALUES ('평상시', '平常');
-INSERT INTO status (status_name, status_name_ja) VALUES ('안전', '安全');
+INSERT INTO status (status_name, status_name_ja) VALUES ('안전', '無事');
 INSERT INTO status (status_name, status_name_ja) VALUES ('대피중', '避難中');
-INSERT INTO status (status_name, status_name_ja) VALUES ('위험', '緊急');
+INSERT INTO status (status_name, status_name_ja) VALUES ('도와주세요', '助けて');
